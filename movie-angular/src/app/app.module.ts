ff307b4613ed845ace067f4a86fafe23ea19b585
dtolent <donnamarie.tolentino@yahoo.com>
2018-10-02 21:11:15 -0400

    ADD: routing

--- movie-angular/src/app/app.module.ts
+++ movie-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { InTheatersComponent } from './in-theaters/in-theaters.component';
@@ -9,6 +10,15 @@ import { UsersComponent } from './users/users.component';
 import { AllMoviesComponent } from './in-theaters/all-movies/all-movies.component';
 import { AllGenreComponent } from './in-theaters/all-genre/all-genre.component';
 import { MovieComponent } from './in-theaters/movie/movie.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'intheaters', component: InTheatersComponent },
+  { path: 'ontv', component: OnTvComponent },
+  { path: 'users', component: UsersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +32,8 @@ import { MovieComponent } from './in-theaters/movie/movie.component';
     MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes) //register routes for Angular to recognize
   ],
   providers: [],
   bootstrap: [AppComponent]
